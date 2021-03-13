6858b4df079625ba5827fc8d20ccbcbc0e0a67ef
Moritz Kneilmann <moritz@kneilmann.tech>
2021-03-13 11:45:47 +0100

    Try using fast inv sqrt algo

--- ndrt-lib/src/vector.rs
+++ ndrt-lib/src/vector.rs
@@ -1,5 +1,22 @@
 use crate::Float;
 
+fn fast_inv_sqrt32(n: f32) -> f32 {
+    // Magic number based on Chris Lomont work:
+    // const MAGIC_U32: u32 = 0x5f375a86;
+    // The Original Magic Number:
+    // const MAGIC_32: u32 = 0x5f3759df;
+    const THREEHALFS: f32 = 1.5f32;
+    let x2: f32 = n * 0.5f32;
+    let mut i: u32 = unsafe { std::mem::transmute(n) }; // evil floating point bit level hacking
+    i = 0x5f375a86 - (i >> 1); // what the fuck?
+    let y: f32 = unsafe { std::mem::transmute(i) };
+    let y = y * (THREEHALFS - (x2 * y * y)); // 1st iteration
+
+    // y  = y * ( THREEHALFS - ( x2 * y * y ) ); // 2nd iteration, this can be remove
+
+    y
+}
+
 pub trait Vector:
     Sized
     + Copy
@@ -20,20 +37,19 @@ pub trait Vector:
 
     fn components(&self) -> &[Float];
 
-    fn length(&self) -> Float {
-        let sum_of_squares = self
-            .components()
+    fn sum_of_squares(&self) -> Float {
+        self.components()
             .iter()
             .map(|c| c * c)
-            .fold(0.0, |a, b| a + b);
+            .fold(0.0, |a, b| a + b)
+    }
 
-        Float::sqrt(sum_of_squares)
+    fn length(&self) -> Float {
+        Float::sqrt(self.sum_of_squares())
     }
 
     fn normalize(&self) -> Self {
-        let len = self.length();
-
-        self.div_scalar(len)
+        self.mul_scalar(fast_inv_sqrt32(self.sum_of_squares()))
     }
 
     fn dot(&self, other: &Self) -> Float {
